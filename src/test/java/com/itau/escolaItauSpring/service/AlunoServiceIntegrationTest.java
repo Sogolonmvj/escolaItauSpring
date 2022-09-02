61cc93659213aa5ed09cb122c082b347c7ca273c
Sogolon <sogolonmvj@yahoo.com>
2022-09-02 13:30:30 -0300

    Test: Adicionei testes de integração.

--- src/test/java/com/itau/escolaItauSpring/service/AlunoServiceIntegrationTest.java
+++ src/test/java/com/itau/escolaItauSpring/service/AlunoServiceIntegrationTest.java
@@ -1,21 +1,23 @@
 package com.itau.escolaItauSpring.service;
 
 import com.itau.escolaItauSpring.config.mapper.AlunoMapper;
-import com.itau.escolaItauSpring.controller.AlunoControllerIntegrationTest;
 import com.itau.escolaItauSpring.model.Aluno;
 import com.itau.escolaItauSpring.repository.AlunoRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-class AlunoServiceIntegrationTest extends AlunoControllerIntegrationTest {
+@SpringBootTest
+class AlunoServiceIntegrationTest {
 
-    private final AlunoRepository alunoRepository;
+    @Autowired
+    private AlunoRepository alunoRepository;
 
-    private final AlunoService alunoService;
+    @Autowired
+    private AlunoService alunoService;
 
-    private final AlunoMapper alunoMapper;
+    @Autowired
+    private AlunoMapper alunoMapper;
 
     private static Aluno aluno;
 
@@ -24,17 +26,9 @@ class AlunoServiceIntegrationTest extends AlunoControllerIntegrationTest {
         aluno = new Aluno();
         aluno.setNome("Marcos");
         aluno.setCpf(12345L);
-        aluno.setAtivado(true);
         aluno.setCursos(null);
         aluno.setEndereco(null);
         aluno.setIdade(26);
-    }
-
-    @Autowired
-    AlunoServiceIntegrationTest(AlunoRepository alunoRepository, AlunoService alunoService, AlunoMapper alunoMapper) {
-        this.alunoRepository = alunoRepository;
-        this.alunoService = alunoService;
-        this.alunoMapper = alunoMapper;
     }
 
     @Test
@@ -44,5 +38,34 @@ class AlunoServiceIntegrationTest extends AlunoControllerIntegrationTest {
 
         Assertions.assertEquals(aluno.getCpf(), alunoEncontrado.getCpf());
         Assertions.assertEquals(aluno.getNome(), alunoEncontrado.getNome());
+    }
+
+    @Test
+    @DisplayName("")
+    void countAlunoByAtivado() {
+        Aluno aluno = alunoRepository.save(AlunoServiceIntegrationTest.aluno);
+        alunoService.ativar(aluno.getId());
+        Long quantidadeDeAlunos = alunoRepository.countAlunoByAtivado(true);
+
+        Assertions.assertEquals(1L, quantidadeDeAlunos);
+    }
+
+    @Test
+    void deleteByCpf() {
+        Aluno aluno = alunoRepository.save(AlunoServiceIntegrationTest.aluno);
+        alunoService.ativar(aluno.getId());
+        Long quantidadeDeAlunoSalvos = alunoRepository.countAlunoByAtivado(true);
+        Assertions.assertEquals(1L, quantidadeDeAlunoSalvos);
+        alunoRepository.deleteByCpf(aluno.getCpf());
+        quantidadeDeAlunoSalvos = alunoRepository.countAlunoByAtivado(true);
+        Assertions.assertEquals(0L, quantidadeDeAlunoSalvos);
+
+    }
+
+    @Test
+    void findById() {
+        Aluno aluno = alunoRepository.save(AlunoServiceIntegrationTest.aluno);
+        Assertions.assertEquals(aluno,alunoRepository.findById(aluno.getId()));
+
     }
 }
